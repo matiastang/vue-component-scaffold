f798b7a348708fff83dd183c9bc7ec1c9a124e04
唐道勇 <matias@tangdaoongdeMBP.lan>
2022-01-11 19:32:35 +0800

    feat: - 更新全局声明

--- global.d.ts
+++ global.d.ts
@@ -2,7 +2,7 @@
  * @Author: matiastang
  * @Date: 2021-12-13 10:12:56
  * @LastEditors: matiastang
- * @LastEditTime: 2022-01-05 10:54:43
+ * @LastEditTime: 2022-01-11 19:32:12
  * @FilePath: /dw-vue-components/global.d.ts
  * @Description: 全局组件声明
  */
@@ -10,6 +10,8 @@ declare module 'vue' {
     export interface GlobalComponents {
         DwWechatLogin: typeof import('datumwealth-vue-components')['DwWechatLogin']
         DwDragVerify: typeof import('datumwealth-vue-components')['DwDragVerify']
+        DwPortfolioBg: typeof import('datumwealth-vue-components')['DwPortfolioBg']
+        DwPortfolioIcon: typeof import('datumwealth-vue-components')['DwPortfolioIcon']
     }
 }
 
